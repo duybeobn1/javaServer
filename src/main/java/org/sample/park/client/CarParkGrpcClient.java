5e64a7301a761d9d17a2fe5e8e4622d728224f63
duybeobn1 <duybeobn1@gmail.com>
2024-06-25 17:52:15 +0200

    Update addr

--- src/main/java/org/sample/park/client/CarParkGrpcClient.java
+++ src/main/java/org/sample/park/client/CarParkGrpcClient.java
@@ -1,26 +1,25 @@
-// CarParkGrpcClient.java
-
 package org.sample.park.client;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import io.grpc.StatusRuntimeException;
-
+import org.sample.park.CarParkServiceGrpc;
 import org.sample.park.CommandRequest;
 import org.sample.park.CommandResponse;
-import org.sample.park.CarParkServiceGrpc;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CarParkGrpcClient {
 
-    private static final Logger logger = LoggerFactory.getLogger(CarParkGrpcClient.class);
     private final CarParkServiceGrpc.CarParkServiceBlockingStub blockingStub;
 
     public CarParkGrpcClient() {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("192.168.1.96", 50051)
+        // The address should be configurable, so it can be different for dev and prod environments
+        String serverAddress = System.getenv("PYTHON_SERVER_ADDRESS"); // Fetch the server address from environment variables
+
+        // Hardcoded port
+        int serverPort = 50051;
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort)
                 .usePlaintext()
                 .build();
         blockingStub = CarParkServiceGrpc.newBlockingStub(channel);
@@ -30,17 +29,12 @@ public class CarParkGrpcClient {
         CommandRequest request = CommandRequest.newBuilder()
                 .setCommand(command)
                 .build();
-        logger.info("Sending CommandRequest to gRPC server with command: {}", command);
         try {
             CommandResponse response = blockingStub.processCommand(request);
-            logger.info("Received response from gRPC server: {}", response.getResult());
             return response.getResult();
-        } catch (StatusRuntimeException e) {
-            logger.error("gRPC request failed: {}", e.getStatus(), e);
-            return "gRPC request failed: " + e.getStatus();
         } catch (Exception e) {
-            logger.error("Unexpected error during gRPC request", e);
-            return "Unexpected error: " + e.getMessage();
+            e.printStackTrace();
+            return "Error: " + e.getMessage();
         }
     }
 }
